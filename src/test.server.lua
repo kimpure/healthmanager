3c7b713481cf14b531f3827ab1e883886a1cbcc4
sunwoo_0823 <robloxstudiopure@gmail.com>
2024-07-06 18:28:16 +0900

    Make TraumaHealth and TraumaDamege

--- src/test.server.lua
+++ src/test.server.lua
@@ -1,7 +1,12 @@
+--!strict
+
 local Players = game:GetService("Players")
 local a = require(script.Parent)
 
 Players.PlayerAdded:Connect(function(player)
     a:Init(player)
-    a:TakeDamege(player , 100)
+    a:TakeDamege(player , 33)
+    a:TakeTraumaDamege(player , 70)
+    a:GetHealth(player , 'TraumaHealth')
+    print(a:GetHealth(player , 'Health'))
 end)
